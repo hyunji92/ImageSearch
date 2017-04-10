709648dba9efc90ef676c86e5d6114ddf2298aa1
jeonghyeonji <chunghj09@gmail.com>
2017-04-10 19:03:17 +0900

    MVP적용 전 .검색어가 없을때, 검색도중 에러가났을때 처리

--- app/src/main/java/dev/jeonghyeonji/imagesearch/view/main/MainActivity.kt
+++ app/src/main/java/dev/jeonghyeonji/imagesearch/view/main/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         // 검색어를 쓰고 1 초가 지나면 검색을 진행하도록 한다
         subject
                 .debounce(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { connect(it) }
 
         // RecyclerView setting
@@ -82,35 +83,44 @@ class MainActivity : AppCompatActivity() {
 
     //network test
     fun connect(searchWord: String) {
-        imageRestClient.client.getImage("12b2bc62862e146f32b943d661b9eda2", searchWord)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        {
-                            Log.d("Test", "Image Add $it")
 
-                            (recyclerView.adapter as ImageAdapter).imageList.clear()
-                            (recyclerView.adapter as ImageAdapter).imageList.addAll(it.channel.item)
-                            (recyclerView.adapter as ImageAdapter).notifyDataSetChanged()
+        if (searchWord.isNullOrBlank()) {
 
-                        },
-                        {
-                            Log.d("Test", "onError $it")
+            Toast.makeText(baseContext, "검색된 이미지가 없습니다", Toast.LENGTH_SHORT).show()
+            (recyclerView.adapter as ImageAdapter).imageList.clear()
+            (recyclerView.adapter as ImageAdapter).notifyDataSetChanged()
 
-                            Toast.makeText(baseContext, "검색된 이미지가 없습니다", Toast.LENGTH_SHORT).show()
-                            (recyclerView.adapter as ImageAdapter).imageList.clear()
-                            (recyclerView.adapter as ImageAdapter).notifyDataSetChanged()
+        } else {
 
-                        },
-                        {
-                            Log.d("Test", "Complete")
-                            val view = this.getCurrentFocus()
-                            if (view != null) {
-                                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                                imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
-                            }
-                        })
+            imageRestClient.client.getImage("12b2bc62862e146f32b943d661b9eda2", searchWord)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            {
+                                Log.d("Test", "Image Add $it")
 
+                                (recyclerView.adapter as ImageAdapter).imageList.clear()
+                                (recyclerView.adapter as ImageAdapter).imageList.addAll(it.channel.item)
+                                (recyclerView.adapter as ImageAdapter).notifyDataSetChanged()
+
+                            },
+                            {
+                                Log.d("Test", "onError $it")
+                                Toast.makeText(baseContext, "이미지를 불러오는데 문제가 발생했습니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show()
+                                (recyclerView.adapter as ImageAdapter).imageList.clear()
+                                (recyclerView.adapter as ImageAdapter).notifyDataSetChanged()
+
+                            },
+                            {
+                                Log.d("Test", "Complete")
+                                val view = this.getCurrentFocus()
+                                if (view != null) {
+                                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+                                }
+                            })
+
+        }
 
     }
 
